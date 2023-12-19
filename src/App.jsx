f59ddbef6aa69ac2d4c9bea59212fd788181dadb
djon198360 <le1907@mail.ru>
2023-12-19 23:19:47 +0300

    feat: create app and install fr

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { RenderNavBarComponent } from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <StrictMode>
+      <RenderNavBarComponent />
+    </StrictMode>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import * as S from "./Style";
+
+export function RenderNavBarComponent() {
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.Header />
+      </S.Container>
+    </S.Wrapper>
+  );
+}
